696d12e89ad376beb7e0ee7eee87a35215d202e0
AleksKen <aleks.ken2017@mail.ru>
2024-08-15 19:31:36 +0300

    Сокращение кода

--- app/src/main/java/hexlet/code/OptionSection.java
+++ app/src/main/java/hexlet/code/OptionSection.java
@@ -4,17 +4,19 @@ import picocli.CommandLine.Parameters;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
-@Command(name = "gendiff", description = "Compares two configuration files and shows a difference.")
+import java.io.File;
+
+@Command(version = {
+        "@|blue Versioned Command 1.0|@",
+        "@|red,bg(white) (c) 2024|@"},
+        name = "gendiff", description = "Compares two configuration files and shows a difference.",
+        mixinStandardHelpOptions = true)
 public class OptionSection {
-        @Parameters(paramLabel = "filepath1", description = "path to first file")
-        String filepath1;
-        @Parameters(paramLabel = "filepath2", description = "path to second file")
-        String filepath2;
-        @Option(names = {"-h", "--help"}, description = "Show this help message and exit.", usageHelp=true)
-        boolean usageHelpRequested;
-        @Option(names = {"-V", "--version"}, description = "Print version information and exit.", versionHelp=true)
-        boolean versionInfoRequested;
-        @Option(names = {"-f", "--format"}, paramLabel = "format",
-                defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
-        String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    File filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    File filepath2;
+    @Option(names = {"-f", "--format"}, paramLabel = "format",
+            defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
+    String format;
 }
