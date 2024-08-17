142c62858ba9e186657894ca521938fbd437038d
AleksKen <aleks.ken2017@mail.ru>
2024-08-17 13:42:46 +0300

    Вывод в json (вывод в строку)

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -9,7 +9,6 @@ import java.util.Map;
 public class Json {
     public static String getResult(Map<String, Description<Object>> descriptionKeys) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        String res = objectMapper.writeValueAsString(descriptionKeys);
-        return res;
+        return objectMapper.writeValueAsString(descriptionKeys);
     }
 }
