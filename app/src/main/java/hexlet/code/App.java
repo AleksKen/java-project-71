6e4e8de058a86c88226b69182d73948887164a14
AleksKen <aleks.ken2017@mail.ru>
2024-08-17 14:55:55 +0300

    Парсинг

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -11,14 +11,14 @@ import java.util.concurrent.Callable;
         name = "gendiff",
         description = "Compares two configuration files and shows a difference.",
         mixinStandardHelpOptions = true)
-public class App implements Callable<Void> {
+public final class App implements Callable<Void> {
     @Parameters(paramLabel = "filepath1", description = "path to first file")
-    String filepath1;
+    private String filepath1;
     @Parameters(paramLabel = "filepath2", description = "path to second file")
-    String filepath2;
+    private String filepath2;
     @Option(names = {"-f", "--format"}, paramLabel = "format",
             defaultValue = "stylish", description = "output format [default: ${DEFAULT-VALUE}]")
-    String format;
+    private String format;
 
     @Override
     public Void call() throws Exception {
