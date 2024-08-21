c6a2cc36a51ac13bb0701a800a5b5633efb5f485
AleksKen <aleks.ken2017@mail.ru>
2024-08-21 10:42:24 +0300

    Тесты без явного указания форматтера

---
+++ app/src/test/java/hexlet/code/DefaultTests.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DefaultTests {
+    @Test
+    public void testGetDiffJson() throws Exception {
+        String filePath1 = "src/test/resources/input-json/file1.json";
+        String filePath2 = "src/test/resources/input-json/file2.json";
+        String filePathExp = "src/test/resources/expected/stylish.txt";
+        String expectedDiff = Files.lines(Paths.get(filePathExp))
+                .collect(Collectors.joining("\n"));
+        String diff = Differ.generate(filePath1, filePath2);
+        assertEquals(expectedDiff, diff);
+    }
+
+    @Test
+    public void testGetDiffYaml() throws Exception {
+        String filePath1 = "src/test/resources/input-yaml/file1.yml";
+        String filePath2 = "src/test/resources/input-yaml/file2.yml";
+        String filePathExp = "src/test/resources/expected/stylish.txt";
+        String expectedDiff = Files.lines(Paths.get(filePathExp))
+                .collect(Collectors.joining("\n"));
+        String diff = Differ.generate(filePath1, filePath2);
+        assertEquals(expectedDiff, diff);
+    }
+}
